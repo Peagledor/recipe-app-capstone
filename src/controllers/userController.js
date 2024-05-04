9b08030dedc1fc017385338fdeeb759b73a36186
Jorge Peraza <j.peraza.22@gmail.com>
2024-05-04 01:08:27 -0600

    styled profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-export const loginUser = async (credentials, navigate) => {
+export const loginUser = async (credentials ) => {
   try {
     const response = await axios.post("http://localhost:4000/users/login", credentials);
-    navigate('/')
     return response.data;
   } catch (error) {
     console.error("Failed to login:", error);
